87a6e25a6085c1fbd4e53613a9db2b36885f814f
Lance Douglas <ldouglastech@gmail.com>
2016-02-09 09:04:37 -0500

    Tryin Scanners, next project = Calc.

---
+++ Scanner/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Inputs");
+        Scanner scn = new Scanner (System.in);
+        scn.nextLine();
+        String stuff = scn.nextLine();
+        System.out.println(stuff);
+    }
+}
