13ef38d4a6f401c170f29a919846dd073d21db56
Lance Douglas <ldouglastech@gmail.com>
2016-02-09 11:37:36 -0500

    Redoable.

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -9,46 +9,65 @@ public class Main {
         String S = "Subtraction";
         String M = "Multiplication";
         String D = "Division";
-        String userinput = scn.nextLine();
-        if (userinput.equals(A)){
-            System.out.println("Lets do some addition!");
-            System.out.println(" ");
-            System.out.println("First number? ");
-            Double num1 = scn.nextDouble();
-            System.out.println("Second number? ");
-            Double num2 = scn.nextDouble();
-            Double num3 = num1 + num2;
-            System.out.println(" ");
-            System.out.println("The answer is " + num3);
-        } else if (userinput.equals(S)){
-            System.out.println("Lets do some subtraction!");
-            System.out.println(" ");
-            System.out.println("First number? ");
-            Double num1 = scn.nextDouble();
-            System.out.println("Second number? ");
-            Double num2 = scn.nextDouble();
-            Double num3 = num1 - num2;
-            System.out.println(" ");
-            System.out.println("The answer is " + num3);
-        } else if (userinput.equals(M)) {
-            System.out.println("Lets do some multiplication!");
-            System.out.println(" ");
-            System.out.println("First number? ");
-            Double num1 = scn.nextDouble();
-            System.out.println("Second number? ");
-            Double num2 = scn.nextDouble();
-            Double num3 = num1 * num2;
-            System.out.println(" ");
-            System.out.println("The answer is " + num3);
-        } else if (userinput.equals(D)) {
-            System.out.println(" ");
-            System.out.println("First number? ");
-            Double num1 = scn.nextDouble();
-            System.out.println("Second number? ");
-            Double num2 = scn.nextDouble();
-            Double num3 = num1 / num2;
-            System.out.println(" ");
-            System.out.println("The answer is " + num3);
+        while(true) {
+            String userinput = scn.nextLine();
+            if (userinput.equals(A)) {
+                System.out.println("Lets do some addition!");
+                System.out.println(" ");
+                System.out.println("First number? ");
+                Double num1 = scn.nextDouble();
+                System.out.println("Second number? ");
+                Double num2 = scn.nextDouble();
+                Double num3 = num1 + num2;
+                System.out.println(" ");
+                System.out.println("The answer is " + num3);
+            } else if (userinput.equals(S)) {
+                System.out.println("Lets do some subtraction!");
+                System.out.println(" ");
+                System.out.println("First number? ");
+                Double num1 = scn.nextDouble();
+                System.out.println("Second number? ");
+                Double num2 = scn.nextDouble();
+                Double num3 = num1 - num2;
+                System.out.println(" ");
+                System.out.println("The answer is " + num3);
+            } else if (userinput.equals(M)) {
+                System.out.println("Lets do some multiplication!");
+                System.out.println(" ");
+                System.out.println("First number? ");
+                Double num1 = scn.nextDouble();
+                System.out.println("Second number? ");
+                Double num2 = scn.nextDouble();
+                Double num3 = num1 * num2;
+                System.out.println(" ");
+                System.out.println("The answer is " + num3);
+            } else if (userinput.equals(D)) {
+                System.out.println(" ");
+                System.out.println("First number? ");
+                Double num1 = scn.nextDouble();
+                System.out.println("Second number? ");
+                Double num2 = scn.nextDouble();
+                Double num3 = num1 / num2;
+                System.out.println(" ");
+                System.out.println("The answer is " + num3);
+            } else
+                continue;
+            System.out.println("Do you want to do another?");
+            Scanner sxm = new Scanner(System.in);
+            String option = sxm.nextLine();
+            if(option.equals("Yes")) {
+               System.out.println("Well I don't know how to do that yet, so fuck off.");
+                return;
+            } else if(option.equals("No")) {
+                System.out.println("Have a nice day.");
+                return;
+            } else {
+                System.out.println("Don't be retarded and type Yes or No");
+                return;
+            }
         }
+
+
+
     }
 }
