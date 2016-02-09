d30cef5e6fda3ed64230b9fd828650630eecfd6e
Lance Douglas <ldouglastech@gmail.com>
2016-02-09 09:24:11 -0500

    Better calc

--- Scanner/src/Main.java
+++ Scanner/src/Main.java
@@ -4,10 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First number: ");
         Scanner scn = new Scanner (System.in);
-        int a1 = scn.nextInt();
+        long a1 = scn.nextLong();
         System.out.println("Second number: ");
-        int a2 = scn.nextInt();
-        int a3 = a1 + a2;
-        System.out.println(a3);
+        long a2 = scn.nextLong();
+        long a3 = a1 + a2;
+        System.out.println(" ");
+        System.out.println("This answer is: " + a3);
     }
 }
