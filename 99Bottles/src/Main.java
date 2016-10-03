b08d8f33801ae509d5169957f00e6a36f84908b8
Lance Douglas <ldouglastech@gmail.com>
2016-10-03 10:11:45 -0400

    Prettied up 99Bottles and removed Kotlin from it.

--- 99Bottles/src/Main.java
+++ 99Bottles/src/Main.java
@@ -1,18 +1,15 @@
-package thebottle.song;
-
 public class Main {
-    
-    public static void main(String[] args) {
-        int beerNum = 99;
-        String word = "bottles";
-        String constant;
-        while (beerNum > 0) {
-            if (beerNum == 1) word = "bottle";
-            constant = beerNum + " " + word + " of beer";
-            System.out.println(constant + " on the wall.\n" + constant + ".");
-            System.out.println("Take one down.\nPass it around.");
-            beerNum = beerNum - 1;
-            if (beerNum == 0) System.out.println("I'm drunk as fuck.");
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int beerNum = 99; beerNum >= 0; beerNum--) {
+            String bottle = " bottles ";
+            if (beerNum == 1) {
+                bottle = " bottle ";
+            }
+            int beerDown = beerNum - 1;
+            System.out.println(beerNum + bottle + "of beer on the wall, " + beerNum + bottle + "of beer.");
+            System.out.println("Take one down, pass it around, " + beerDown + " bottles of beer on the wall.\n");
+            Thread.sleep(1500);
         }
     }
 }
