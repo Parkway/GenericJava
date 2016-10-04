3e32338b42537a983c989b02104e6f7158d112a0
Lance Douglas <ldouglastech@gmail.com>
2016-10-03 21:43:36 -0400

    Constructors are pretty cool.

--- OOPCheese/src/BagOfCheese.java
+++ OOPCheese/src/BagOfCheese.java
@@ -3,4 +3,12 @@ public class BagOfCheese {
     double weight;
     int daysAged;
     boolean isDomestic;
+
+    public BagOfCheese(String kind, double weight,
+                       int daysAged, boolean isDomestic) {
+        this.kind = kind;
+        this.weight = weight;
+        this.daysAged = daysAged;
+        this.isDomestic = isDomestic;
+    }
 }
--- OOPCheese/src/CreateBags.java
+++ OOPCheese/src/CreateBags.java
@@ -2,45 +2,25 @@ import javax.swing.JOptionPane;
 
 public class CreateBags {
     public static void main(String[] args) {
-        BagOfCheese bag1 = new BagOfCheese();
-        bag1.kind = "Cheddar";
-        bag1.weight = 2.43;
-        bag1.daysAged = 30;
-        bag1.isDomestic = true;
-
-        BagOfCheese bag2 = new BagOfCheese();
-        bag2.kind = "Blue";
-        bag2.weight = 5.987;
-        bag2.daysAged = 90;
-        bag2.isDomestic = false;
-
+        BagOfCheese bag0 = new BagOfCheese("Muenster", 2.65, 30, true);
+        BagOfCheese bag1 = new BagOfCheese("Cheddar", 2.43, 30, true);
+        BagOfCheese bag2 = new BagOfCheese("Blue",5.987,90, false);
+        displayBags(bag0);
         displayBags(bag1);
         displayBags(bag2);
-
         other();
     }
-
+    public static void other() {
+        BagOfCheese bag = new BagOfCheese("Pepperjack", 5.67, 90, true);
+        displayBags(bag);
+        bag = new BagOfCheese("Colby", 9.23, 1000, true);
+        displayBags(bag);
+    }
     private static void displayBags(BagOfCheese bag) {
         JOptionPane.showMessageDialog(null,
                 bag.kind + ", " +
-                bag.weight + ", " +
-                bag.daysAged + ", " +
-                bag.isDomestic);
-    }
-    public static void other() {
-        BagOfCheese bag = new BagOfCheese();
-        bag.kind = "Pepperjack";
-        bag.weight = 5.00;
-        bag.daysAged = 15;
-        bag.isDomestic = true;
-        displayBags(bag);
-
-        bag = new BagOfCheese();
-
-        bag.kind = "Colby";
-        bag.weight = 1.5;
-        bag.daysAged = 45;
-        bag.isDomestic = false;
-        displayBags(bag);
+                        bag.weight + ", " +
+                        bag.daysAged + ", " +
+                        bag.isDomestic);
     }
 }
