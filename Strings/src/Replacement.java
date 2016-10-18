6185ebac12692df289bf99f6b10eafe9557545af
Lance Douglas <ldouglastech@gmail.com>
2016-10-18 12:00:53 -0400

    I like this one a lot. Date and Time class created, but Replacement is where it's at.

---
+++ Strings/src/Replacement.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Replacement {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("This program allows you to replace input with other input.\n" +
+                "Be aware, input is case sensitive. example != eXaMPle.");
+        System.out.print("Enter a phrase: ");
+        String phrase = scn.nextLine();
+        System.out.println("What character would you like to replace?");
+        String let = scn.nextLine();
+        if (phrase.contains(let)) {
+            System.out.println("What would you like to replace it with?");
+            String newLet = scn.nextLine();
+            String newPhrase = phrase.replace(let, newLet);
+            System.out.println("\nOriginal String: " + phrase);
+            System.out.println("New String: " + newPhrase);
+        } else {
+            System.out.println("Your input does not contain " + let + ".\n" +
+                    "Unable to process.");
+            System.exit(0);
+        }
+
+    }
+}
+
