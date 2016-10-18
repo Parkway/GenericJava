b569230fa0eb9ebcf752f1ac05ba988e9b237bba
Lance Douglas <ldouglastech@gmail.com>
2016-10-18 11:05:02 -0400

    Multiple range completed.

--- Strings/src/UnlimitedIndex.java
+++ Strings/src/UnlimitedIndex.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
 
 public class UnlimitedIndex {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner scn = new Scanner(System.in);
         System.out.println("This program allows you to pull certain characters from a string,\n" +
                 "based on where they are located in a 0-*phrase-length* count.");
         System.out.print("Please enter a phrase: ");
         String phrase = scn.nextLine();
         int phraseLen = phrase.length();
+        System.out.println("Keep the numbers below " + phraseLen + ", and enter in ascending order.");
         System.out.print("Please enter a number.\nThis is the amount of chars you want to extract: ");
         int amount = scn.nextInt();
-        System.out.print("Now, what index locations do you want to extract?\n(Keep it below " + phraseLen + "): ");
-        int charIndex = scn.nextInt();
+        System.out.print("Now, what index locations do you want to extract: ");
 
         while (amount > 0) {
+            int charIndex = scn.nextInt() - 1;
             int charNum = phrase.charAt(charIndex);
-            System.out.println((char)charIndex);
+            System.out.print("'" + (char)charNum + "' ");
+            Thread.sleep(500);
             amount--;
         }
     }
