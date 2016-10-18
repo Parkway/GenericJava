cc1f84d6c9e672809d4b4d57c5b20e6d097e84ed
Lance Douglas <ldouglastech@gmail.com>
2016-10-18 10:15:54 -0400

    Added better text.

--- Strings/src/StringRange.java
+++ Strings/src/StringRange.java
@@ -8,14 +8,20 @@ public class StringRange {
         int phraseLength = phrase.length();
         System.out.println("Now, enter two numbers; both less than the total number\n" +
                 "of characters in your phrase, at " + phraseLength + " characters");
-        int num1 = scn.nextInt();
-        num1 = num1 - 1;
-        int num2 = scn.nextInt();
-        int max = Math.max(num1, num2);
-        int min = Math.min(num1, num2);
-        for (int Char = min; Char < max; Char++) {
-            int range = phrase.charAt(Char);
-            System.out.print((char)range);
+        try {
+            int num1 = scn.nextInt();
+            num1 = num1 - 1;
+            int num2 = scn.nextInt();
+            int max = Math.max(num1, num2);
+            int min = Math.min(num1, num2);
+            System.out.print("You have selected: '");
+            for (int Char = min; Char < max; Char++) {
+                int range = phrase.charAt(Char);
+                System.out.print((char) range);
+            }
+            System.out.print("'");
+        } catch (StringIndexOutOfBoundsException SIOOBE) {
+            System.out.println("One of the numbers is higher than " + phraseLength + ". How did you fuck that up?");
         }
     }
 }
