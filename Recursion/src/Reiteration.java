66a4a4b07593b27ea96dacf1e52dfadad65b0402
Lance Douglas <ldouglastech@gmail.com>
2016-10-19 15:10:22 -0400

    Learning recursion is frustrating.

---
+++ Recursion/src/Reiteration.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Reiteration {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scn.nextInt();
+        displaySharp(num);
+    }
+    public static void displaySharp(int num) {
+        while (num > 0) {
+            System.out.print("#");
+            num--;
+        }
+        System.out.println();
+    }
+}
