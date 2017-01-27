25d24827bd447d15ac14c6fc5564f59991b550f5
Lance Douglas <ldouglastech@gmail.com>
2017-01-27 09:49:14 -0500

    Added three more radioButtons.

--- GUI/src/textTool.java
+++ GUI/src/textTool.java
@@ -5,36 +5,47 @@ import java.awt.event.ActionListener;
 
 public class textTool extends JFrame {
 
-    JPanel Pnl = new JPanel();
-    JTextArea JTA = new JTextArea();
+    private JPanel Pnl = new JPanel();
+    private JTextArea JTA = new JTextArea();
 
-    JRadioButton JRB1 = new JRadioButton("Red");
-    JRadioButton JRB2 = new JRadioButton("Blue");
-    JRadioButton JRB3 = new JRadioButton("Black");
+    private JRadioButton JRB1 = new JRadioButton("Red");
+    private JRadioButton JRB2 = new JRadioButton("Blue");
+    private JRadioButton JRB3 = new JRadioButton("Black");
+    private JRadioButton JRB4 = new JRadioButton("Plain");
+    private JRadioButton JRB5 = new JRadioButton("Bold");
+    private JRadioButton JRB6 = new JRadioButton("Italic");
 
-    //TODO: Add three more radioButtons for PLAIN, BOLD, ITALIC font choices. Can't wait!
-
-    ButtonGroup BG = new ButtonGroup();
+    private ButtonGroup Clrs = new ButtonGroup();
+    private ButtonGroup Fnts = new ButtonGroup();
 
     private textTool() {
         super("Text Tool");
         setSize(500,250);
 
         add(Pnl);
-        Pnl.add(JTA, BorderLayout.SOUTH);
+        Pnl.add(JTA, BorderLayout.NORTH);
         Pnl.add(JRB1, BorderLayout.EAST);
         Pnl.add(JRB2, BorderLayout.EAST);
         Pnl.add(JRB3, BorderLayout.EAST);
-        BG.add(JRB1);
-        BG.add(JRB2);
-        BG.add(JRB3);
+        Pnl.add(JRB4, BorderLayout.SOUTH);
+        Pnl.add(JRB5, BorderLayout.SOUTH);
+        Pnl.add(JRB6, BorderLayout.SOUTH);
+
+        Clrs.add(JRB1);
+        Clrs.add(JRB2);
+        Clrs.add(JRB3);
+
+        Fnts.add(JRB4);
+        Fnts.add(JRB5);
+        Fnts.add(JRB6);
+
         Pnl.setBackground(Color.white);
 
         JTA.setBackground(Color.white);
         JTA.setSize(450, 400);
         JTA.setLineWrap(true);
         JTA.setWrapStyleWord(true);
-        JTA.setFont(new Font("Ubuntu", Font.ITALIC, 20));
+        JTA.setFont(new Font("Ubuntu", Font.PLAIN, 20));
 
 
         JRB1.addActionListener(new ActionListener() {
@@ -53,6 +64,24 @@ public class textTool extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JTA.setForeground(Color.black);
+            }
+        });
+        JRB4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTA.setFont(new Font("Ubuntu", Font.PLAIN, 20));
+            }
+        });
+        JRB5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTA.setFont(new Font("Ubuntu", Font.BOLD, 20));
+            }
+        });
+        JRB6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTA.setFont(new Font("Ubuntu", Font.ITALIC, 20));
             }
         });
 
