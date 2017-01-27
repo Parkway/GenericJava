80a8d68287f5fe80b28af2a71e4e731d22786beb
Lance Douglas <ldouglastech@gmail.com>
2017-01-26 20:59:20 -0500

    New class to test File Template

---
+++ GUI/src/Buttons.java
@@ -0,0 +1,6 @@
+/********************************************
+ * Created by Lance Douglas on 1/26/17
+ *********************************************/
+public class Buttons {
+    //TODO: Literally everything. This was just to test a default file template (Yeah at the top, that.)
+}
