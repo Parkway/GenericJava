7163bffd92c06c3f4f8e138d25dd081f42204d9c
Lance Douglas <ldouglastech@gmail.com>
2017-01-27 22:15:48 -0500

    Commented and defined some code.

--- GUI/src/Colors.java
+++ GUI/src/Colors.java
@@ -3,13 +3,13 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Colors extends JFrame {
-    JPanel Frame = new JPanel();
+    private JPanel Frame = new JPanel();
 
-    JRadioButton JRB1 = new JRadioButton("Red");
-    JRadioButton JRB2 = new JRadioButton("Green");
-    JRadioButton JRB3 = new JRadioButton("Blue");
+    private JRadioButton JRB1 = new JRadioButton("Red");
+    private JRadioButton JRB2 = new JRadioButton("Green");
+    private JRadioButton JRB3 = new JRadioButton("Blue");
 
-    ButtonGroup Colors = new ButtonGroup();
+    private ButtonGroup Colors = new ButtonGroup();
 
     private Colors() {
         super("Color Switcher");
@@ -24,33 +24,23 @@ public class Colors extends JFrame {
         Colors.add(JRB2);
         Colors.add(JRB3);
 
-        JRB1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        JRB1.addActionListener((ActionEvent e) -> {
                 JRB1.setBackground(Color.RED);
                 JRB2.setBackground(Color.RED);
                 JRB3.setBackground(Color.RED);
                 Frame.setBackground(Color.RED);
-            }
         });
-        JRB2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        JRB2.addActionListener((ActionEvent e) -> {
                 JRB1.setBackground(Color.GREEN);
                 JRB2.setBackground(Color.GREEN);
                 JRB3.setBackground(Color.GREEN);
                 Frame.setBackground(Color.GREEN);
-
-            }
         });
-        JRB3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        JRB3.addActionListener((ActionEvent e) -> {
                 JRB1.setBackground(Color.BLUE);
                 JRB2.setBackground(Color.BLUE);
                 JRB3.setBackground(Color.BLUE);
                 Frame.setBackground(Color.BLUE);
-            }
         });
 
         setVisible(true);
--- GUI/src/Counter.java
+++ GUI/src/Counter.java
@@ -1,14 +1,18 @@
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /********************************************
  * Created by Lance Douglas on 1/26/17
  *********************************************/
 
 public class Counter extends JFrame {
-    //TODO: Literally everything. This was just to test a default file template (Yeah at the top, that.)
+
+    //TODO: Perhaps implement FlowLayout? Eh, I should learn how to use it first.
+
+    //Adding ints, strings, and Panel stuff.
+    //private because they can be and goodCodingPractices™
 
     private int clickCount = 0;
     private int totalClicks = 0;
@@ -27,13 +31,19 @@ public class Counter extends JFrame {
         super("Clicker Counter");
         setSize(250,250);
 
+        //Adding all elements to the JPanel
+
         add(Pnl);
-        Pnl.add(Lbl, BorderLayout.CENTER);
-        Pnl.add(adder, BorderLayout.NORTH);
-        Pnl.add(miner, BorderLayout.SOUTH);
-        Pnl.add(total, BorderLayout.CENTER);
-        Pnl.add(reset, BorderLayout.SOUTH);
+        Pnl.add(Lbl);
+        Pnl.add(adder);
+        Pnl.add(miner);
+        Pnl.add(total);
+        Pnl.add(reset);
         Pnl.setBackground(Color.white);
+
+        /*  Hard coded location for buttons and textAreas. Bad coding practices!
+            Don't do this, it's just for educational purposes and practice
+            While I learn how to make it not shitty. */
 
         Lbl.setSize(50,50);
         Lbl.setBounds(80,140,150,10);
@@ -55,28 +65,47 @@ public class Counter extends JFrame {
         reset.setBackground(Color.white);
         reset.setBounds(15, 200,225,20);
 
-        adder.addActionListener((ActionEvent e) -> {
+        /* Today, I learned about the magic of Lambda expressions. They're my new best friend.
+           They turn this hideous conglomeration (once known as the Dixie Chicks):
+
+            * adder.addActionListener(new ActionListener() {
+            * @Override
+            * public void actionPerformed(ActionEvent AE) {
+            *
+            * }
+            * });
+
+           Into this gorgeous succinct piece of art:
+
+            * adder.addActionListener((ActionEvent AE) -> {
+            *
+            * });
+
+           Moral of the story they make me happy.
+         */
+
+        adder.addActionListener((ActionEvent AE) -> { //Now this here is some facking magic. Lambda expressions are the bomb.
                 clickCount++;
                 totalClicks++;
-                String newClick = Integer.toString(clickCount);
+                String newClick = Integer.toString(clickCount); //Converts the clickCount Int to a String - necessary.
                 Lbl.setText(counterIndex + newClick);
                 total.setText(totalCount + totalClicks);
         });
-        miner.addActionListener((ActionEvent e) -> {
+        miner.addActionListener((ActionEvent AE) -> {
                 clickCount--;
                 totalClicks++;
                 String newClick = Integer.toString(clickCount);
                 Lbl.setText(counterIndex + newClick);
                 total.setText(totalCount + totalClicks);
         });
-        reset.addActionListener((ActionEvent e) -> {
-                clickCount = 0;
-                totalClicks = 0;
+        reset.addActionListener((ActionEvent AE) -> {
+                clickCount = zero;
+                totalClicks = zero; //Else it keeps counting.
                 Lbl.setText(counterIndex + zero);
                 total.setText(totalCount + zero);
         });
 
-        setVisible(true);
+        setVisible(true); //Invisible GUI == Bad GUI.
     }
     public static void main(String[] args) {
         Counter GUI = new Counter();
