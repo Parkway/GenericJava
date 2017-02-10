3f86b3b96b0c9bcf7cd631c54c90240e6c6a663e
Parkway <ldouglastech@gmail.com>
2017-02-10 00:33:54 -0500

    Just for setting up new device.

---
+++ howToGitHub/src/Explain.java
@@ -0,0 +1,19 @@
+/*
+ * So, the issue I was having is that whenever I clone a repo
+ * from GitHub, the only files that I could access from IntelliJ
+ * were the README and "External Libraries". Well, after enough
+ * trial and error (mostly error), I gave up. Posted it on
+ * the IntelliJ User Forum and asked how to fix it, which
+ * can be found here:
+ *
+ * https://intellij-support.jetbrains.com/hc/en-us/community/
+ * posts/115000089044-Cloning-GitHub-Repo-leaves-mostly-empty-folder-
+ *
+ * Turns out, the solution is 
+ *
+ *
+ *
+ *
+ *
+ *
+ */
---
+++ newPcTestRepo/src/tester.java
@@ -0,0 +1,9 @@
+/************************************
+ * Created by Lance Douglas on 2/10/2017
+ ************************************/
+public class tester {
+    //This is just to test github integration.
+    public static void main(String[] args) {
+        //New laptop woo.
+    }
+}
