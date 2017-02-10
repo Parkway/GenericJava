dd5e6c512936798591b20633d62857c837dc3def
Parkway <ldouglastech@gmail.com>
2017-02-10 09:40:47 -0500

    Just in case I have to do this again in the future.

--- howToGitHub/src/Explain.java
+++ howToGitHub/src/Explain.java
@@ -9,11 +9,9 @@
  * https://intellij-support.jetbrains.com/hc/en-us/community/
  * posts/115000089044-Cloning-GitHub-Repo-leaves-mostly-empty-folder-
  *
- * Turns out, the solution is 
- *
- *
- *
- *
- *
- *
+ * Turns out, the solution is adding a Module via Project Structure.
+ * To do this, go to File, Project Structure, Modules, Add New (+),
+ * and Import Module from Existing Sources. New Module makes a
+ * folder, which I don't want. So, Import a Module, locate the src/
+ * folder and mark it as the folder that holds all the java files.
  */
