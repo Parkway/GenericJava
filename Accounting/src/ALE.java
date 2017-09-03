bf56e2a71150e415b60e9a227475bbdb90182438
Parkway <ldouglastech@gmail.com>
2017-09-02 22:04:13 -0400

    Code commented.

--- Accounting/src/ALE.java
+++ Accounting/src/ALE.java
@@ -15,28 +15,35 @@ public class ALE {
     }
         private static void math() {
             try {
-                NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.US);
+                NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.US); //Allows for number formatting
 
                 String liabilities = JOptionPane.showInputDialog("Please enter your total liabilities:");
-                String noComma = liabilities.replace(",", "");
-                String noDolla = noComma.replace("$", "");
-                int liabilint = Integer.parseInt(noDolla);
+                String noChars = liabilities.replace(",", "").replace("$","");
+                //Removes $ and , from inputted numbers, in case of annoying users.
+                int liabilint = Integer.parseInt(noChars); //Converts the above string to an integer.
+
+                //take all parts of equity formula as input and separate by commas
                 String equityParts = JOptionPane.showInputDialog(
                         "Please enter your Owner value, Capital value, Withdrawals, Revenue, and Expenses,\n" +
-                                "separated by commas.");
-                String[] parts = equityParts.split(", ");
-                String own = parts[0];
+                                "separated by commas.\nDo not add dollar signs or commas to the numbers.");
+                String[] parts = equityParts.split(", "); //Takes all input, then splits it by comma location.
+                String own = parts[0]; //First part of equity piece.
                 String capOwn = parts[1];
                 String With = parts[2];
                 String Rev = parts[3];
-                String Exp = parts[4];
+                String Exp = parts[4]; //Last part.
+
+                //Converts each value to integer for calculations.
                 int ownVal = Integer.parseInt(own);
                 int capOwnVal = Integer.parseInt(capOwn);
                 int WithVal = Integer.parseInt(With);
                 int RevVal = Integer.parseInt(Rev);
                 int ExpVal = Integer.parseInt(Exp);
+
+                //Formulae (with an e at the end for fancy)
                 int equity = ownVal + (capOwnVal - ownVal) + (WithVal + (RevVal - ExpVal));
 
+                //Confirmation dialog
                 int correct = JOptionPane.showConfirmDialog(null,
                         "Owner Value: $" + ownVal + "\n" +
                                 "Capital Value: $" + capOwnVal + "\n" +
@@ -44,20 +51,25 @@ public class ALE {
                                 "Revenue: $" + RevVal + "\n" +
                                 "Expenses: $" + ExpVal + "\n" +
                                 "Is this correct?");
+
+                //if correct, math out total asset value
                 if (correct == JOptionPane.YES_OPTION) {
                     int assets = liabilint + equity;
                     String assetFormat = numberFormat.format(assets);
                     JOptionPane.showMessageDialog(null,
                             "Your total assets should be $" + assetFormat + ".");
+
+                //otherwise, try again.
                 } else {
                     JOptionPane.showMessageDialog(null,
                             "Let's try again. :)");
                     math();
                 }
+
+            //Not important, catches goofs in input
             } catch (NumberFormatException NFE) {
                 JOptionPane.showMessageDialog(null,
-                        "You were supposed to enter\n" +
-                                "an integer, nothing else. Try again.");
+                        "Please only enter integers. Try again.");
                 math();
             } catch (ArrayIndexOutOfBoundsException AIOOBE) {
                 JOptionPane.showMessageDialog(null,
