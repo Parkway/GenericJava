c9c20e0118650687144bc6157a48b681b2572532
Parkway <ldouglastech@gmail.com>
2017-09-29 21:28:57 -0400

    Made a password cracker example.

--- Math/src/Multiply.java
+++ Math/src/Multiply.java
@@ -3,13 +3,21 @@ import java.util.Scanner;
 /***************************************************
  * Created by Lance Douglas on 4/29/2017 - Math *
  ***************************************************/
-public class Multiply {
-    public static void main(String[] args) {
+
+public class Multiply { //Class
+    Factorial facts = new Factorial(); //Calls factorial class for use in this class.
+
+    public static void main(String[] args) { //main method
+
         Scanner scn = new Scanner(System.in);
         System.out.println("Please enter two numbers to multiply:");
         int num1 = scn.nextInt();
         int num2 = scn.nextInt();
         int total = num1*num2;
-        System.out.println(num1 + "x" + num2 + "=" + total);
+        print(num1 + "x" + num2 + "=" + total);
+
+    }
+    public static void print(String s) {
+        System.out.println(s);
     }
 }
