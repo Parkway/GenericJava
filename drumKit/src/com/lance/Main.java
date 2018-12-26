2f7a4401598bed033501ac6fa6db48ed81d7c87e
Lance Douglas <ldouglastech@gmail.com>
2018-12-25 23:49:02 -0500

    Commented the ever loving heck out of drumKit.

--- drumKit/src/com/lance/Main.java
+++ drumKit/src/com/lance/Main.java
@@ -1,42 +1,102 @@
+/**************************************************
+ * If I'm gonna get serious about programming,    *
+ * I should get serious about comments.           *
+ **************************************************/
+
 package com.lance;
 
+//Need Scanner for user input.
 import java.util.Scanner;
 
 public class Main {
+
+    /*
+    The point of this method is simply to call drummer().
+    This is done so that at the end of the program, the user
+    can choose to re run the program. The most straightforward
+    way that I know of to do that is simply call the method.
+    As of now, I have no idea if this is bad practice.
+     */
     public static void main(String[] args) {
         drummer();
     }
 
-    static void drummer() {
-        drumKit d = new drumKit();
+    private static void drummer() {
 
+        //New drumKit and Scanner objects.
+        drumKit d = new drumKit();
         Scanner scn = new Scanner(System.in);
 
-        String snareDrum = "snare drum";
+        //Two user input instances
+        String input1, input2;
+
+        //Drum Names
+        String snare = "snare drum";
         String highHat = "high hat";
         String bass = "bass";
 
+        //Drum Sounds
+        String snareSound = "tsst";
+        String highSound = "pop";
+        String bassSound = "boom";
+
+        //Ask user what drum type they want to test
         System.out.println("What drum would you like to hear?" +
-            "\nSnare" +
-            "\nHigh Hat" +
-            "\nBass");
+                "\nSnare" +
+                "\nHigh Hat" +
+                "\nBass");
 
-        String input = scn.nextLine().toLowerCase();
+        /*
+        Take input from user, convert all input to lower case.
+        This approach ensures compatibility with available inputs.
+        */
+        input1 = scn.nextLine().toLowerCase();
 
-        if (input.equals("snare")) {
-            d.drumType = snareDrum;
-            d.playSnare("tsst");
+        /*
+        Each acceptable input corresponds to the
+        requested drum type. When the user types snare,
+        for example, the drumType of d (drumKit) is
+        set to $snare, and passes the $snareSound string.
+        The try{} block is necessary for the Thread.sleep
+        that you can see below.
+        */
+        try {
+            if (input1.equals("snare")) {
+                //Set drumType to snare
+                d.drumType = snare;
+                //Call playSnare method with
+                //snareSound constructors.
+                d.playSnare(snareSound);
+                //Pause execution for 1.5 seconds.
+                Thread.sleep(1500);
+                //Repeat
+                drummer();
+            } else if (input1.equals("high hat")) {
+                d.drumType = highHat;
+                d.playHighHat(highSound);
+                Thread.sleep(1500);
+                drummer();
+            } else if (input1.equals("Bass")) {
+                d.drumType = bass;
+                d.playBass(bassSound);
+                Thread.sleep(1500);
+                drummer();
+            } else {
+                System.out.println("You did not select a correct option." +
+                        "\nWould you like to try again?");
 
-        } else if (input.equals("high hat")) {
-            d.drumType = highHat;
-            d.playHighHat("pop");
+                input2 = scn.nextLine().toLowerCase();
 
-        } else if (input.equals("Bass")) {
-            d.drumType = bass;
-            d.playBass("boom");
-        } else {
-            System.out.println("You did not select a correct option. Restarting.");
-            drummer();
+                if (input2.contains("y")) {
+                    drummer();
+                } else {
+                    System.out.println("Good bye.");
+                    //Exit program.
+                    System.exit(0);
+                }
+            }
+        } catch (InterruptedException IE) {
+            //Doesn't need content.
         }
     }
 }
--- drumKit/src/com/lance/drumKit.java
+++ drumKit/src/com/lance/drumKit.java
@@ -1,19 +1,36 @@
+/*
+I don't know what packages do.
+Something something, make classes work together in subdirectory?
+*/
 package com.lance;
 
 class drumKit {
 
+    /*
+    Declaring drumType String for object oriented showcase.
+    drumType will be set() by Main.java, lines 27, 31, and 35
+     */
     String drumType;
 
+    /*
+    These methods take the String sound from Main.java,
+    as well as the updated drumType value.
+    Then they print out "A $drum makes a $sound sound,"
+    where $drum and $sound are set by Main.
+    */
     void playSnare(String sound) {
-        String sentence = "A " + drumType + " makes a " + sound + " sound.";
+        //A $drum makes a $sound.
+        String sentence = "A " + drumType + " makes a " + sound + " sound.\n";
         System.out.println(sentence);
     }
-    void playHighHat(String sound    ) {
-        String sentence = "A " + drumType + " makes a " + sound + " sound.";
+
+    void playHighHat(String sound) {
+        String sentence = "A " + drumType + " makes a " + sound + " sound.\n";
         System.out.println(sentence);
     }
+
     void playBass(String sound) {
-        String sentence = "A " + drumType + " makes a " + sound + " sound.";
+        String sentence = "A " + drumType + " makes a " + sound + " sound.\n";
         System.out.println(sentence);
     }
 }
