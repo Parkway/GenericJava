721a4f5973240f5d8d5481d2f7f02c1050a6cb7a
Lance Douglas <ldouglastech@gmail.com>
2018-12-28 00:55:23 -0500

    Learned a lot about OOP. Yet to comment, will next.

---
+++ Animals/src/com/lance/Animals.java
@@ -0,0 +1,29 @@
+/***********************************************************************
+ * Created by Lance Douglas on 12/27/2018
+ ***********************************************************************/
+package com.lance;
+
+public class Animals {
+    int age, weight;
+    String name;
+
+    void setName(String name) {
+        this.name = name;
+    }
+    void setAge(int age) {
+        this.age = age;
+    }
+    void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    String getName() {
+        return this.name;
+    }
+    int getAge() {
+        return this.age;
+    }
+    int getWeight() {
+        return this.weight;
+    }
+}
--- Animals/src/com/lance/Main.java
+++ Animals/src/com/lance/Main.java
@@ -3,10 +3,31 @@ package com.lance;
 public class Main {
 
     public static void main(String[] args) {
-        Mammals mammals = new Mammals();
+        Animals [] cats = new Animals[3];
+        int var = 0;
 
-        mammals.setLegs(89);
+        cats[0] = new Animals();
+        cats[1] = new Animals();
+        cats[2] = new Animals();
 
-        System.out.println(mammals.getLegs());
+        cats[0].setName("Harold");
+        cats[1].setName("Jakob");
+        cats[2].setName("Pete");
+
+        cats[0].setAge(2);
+        cats[1].setAge(7);
+        cats[2].setAge(9);
+
+        cats[0].setWeight(9);
+        cats[1].setWeight(15);
+        cats[2].setWeight(13);
+
+
+        while (var < 3) {
+            String cat = cats[var].getName() + ", " + cats[var].getAge() + " years old, "
+                    + cats[var].getWeight() + " pounds.\n";
+            var++;
+            System.out.println(cat);
+        }
     }
 }
